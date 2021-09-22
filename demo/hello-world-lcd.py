c31cc508efba6b27f5797b24234d68300cd97c39
Luca Gandini <lgandini@funambol.com>
2021-09-22 16:50:09 +0200

    Demo for LCD usage

---
+++ demo/hello-world-lcd.py
@@ -0,0 +1,29 @@
+import lcd
+import time
+
+display = lcd.lcd(0x27, 1)
+
+def main():
+  # Main program block
+
+  # Initialise display
+  display.lcd_init()
+
+  while True:
+    # Send some test
+    display.lcd_string("Hello world!", display.LCD_LINE_1)
+    time.sleep(3)
+  
+    # Send some more text
+    display.lcd_string("Raspberry PI", display.LCD_LINE_2)
+
+    time.sleep(3)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    display.lcd_byte(0x01, display.LCD_CMD)
